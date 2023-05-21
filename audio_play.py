bc51e599572b14c19713ede00a57cd906278b5d6
rolin-qq <535565543@qq.com>
2023-05-21 11:09:33 +0800

    modified:   __pycache__/audio_play.cpython-38.pyc 	new file:   __pycache__/audio_record.cpython-39.pyc 	modified:   __pycache__/baidu_ai.cpython-38.pyc 	modified:   audio_play.py 	deleted:    synth.mp3 	modified:   test.wav

--- audio_play.py
+++ audio_play.py
@@ -1,8 +1,8 @@
-# import os
+import os
 
-# def play(file_name):
-#     """audio play"""
-#     # os.system(f"ffplay -autoexit {file_name}")		#播放完毕自动退出
+def play(file_name):
+    """audio play"""
+    os.system(f"ffplay -autoexit {file_name}")		#播放完毕自动退出
 
 
 
@@ -14,22 +14,22 @@
 
 
 '''
-如果您尚未安装 `pydub` 库，请在命令行中使用以下命令进行安装：
+# 如果您尚未安装 `pydub` 库，请在命令行中使用以下命令进行安装：
 
-```
-pip install pydub
-```
+# ```
+# pip install pydub
+# ```
 
-请注意，`pydub` 库需要 FFmpeg 多媒体框架支持才能正常工作，因此还需要安装 FFmpeg。可以在 FFmpeg 官网（https://ffmpeg.org/）
-下载适用于您所使用操作系统的安装包并进行安装。安装完成后，请将 FFmpeg 的 bin 目录添加到系统的环境变量中，以确保 `pydub` 库可以找到并使用 FFmpeg。
-'''
-from pydub import AudioSegment
-from pydub.playback import play as pb_play
+# 请注意，`pydub` 库需要 FFmpeg 多媒体框架支持才能正常工作，因此还需要安装 FFmpeg。可以在 FFmpeg 官网（https://ffmpeg.org/）
+# 下载适用于您所使用操作系统的安装包并进行安装。安装完成后，请将 FFmpeg 的 bin 目录添加到系统的环境变量中，以确保 `pydub` 库可以找到并使用 FFmpeg。
+# '''
+# from pydub import AudioSegment
+# from pydub.playback import play as pb_play
 
-def play(file_name):
-    """audio play"""
-    sound = AudioSegment.from_file(file_name)
-    pb_play(sound)
+# def play(file_name):
+#     """audio play"""
+#     sound = AudioSegment.from_file(file_name)
+#     pb_play(sound)
 
 
 
